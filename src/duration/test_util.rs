fa297697d051c5895c01dfaa2ba8814e3600643c
Stephen A. Imhoff <clockwork-muse@outlook.com>
2020-01-26 11:49:10 -0800

    Change util to use impl Strategy

--- src/duration/test_util.rs
+++ src/duration/test_util.rs
@@ -4,7 +4,7 @@ use crate::constants::*;
 
 use crate::Duration;
 
-pub fn arb_duration() -> BoxedStrategy<Duration> {
+pub fn arb_duration() -> impl Strategy<Value = Duration> {
     (any::<i64>(), 0..NANOSECONDS_IN_SECOND)
         .prop_map(|(seconds, nanos)| Duration::of_seconds_and_adjustment(seconds, nanos))
         .boxed()
