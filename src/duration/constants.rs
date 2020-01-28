2cd1a646b7fb9c6a6a8672e472af81c59ad46a07
Stephen A. Imhoff <clockwork-muse@outlook.com>
2020-01-27 20:57:12 -0800

    Add missing semi-colons

--- src/duration/constants.rs
+++ src/duration/constants.rs
@@ -8,19 +8,19 @@ use crate::Duration;
 fn max() {
     let duration = Duration::MAX;
     assert_eq!(i64::MAX, duration.seconds());
-    assert_eq!(NANOSECONDS_IN_SECOND as u32 - 1, duration.nano())
+    assert_eq!(NANOSECONDS_IN_SECOND as u32 - 1, duration.nano());
 }
 
 #[test]
 fn zero() {
     let duration = Duration::ZERO;
     assert_eq!(0i64, duration.seconds());
-    assert_eq!(0u32, duration.nano())
+    assert_eq!(0u32, duration.nano());
 }
 
 #[test]
 fn min() {
     let duration = Duration::MIN;
     assert_eq!(i64::MIN, duration.seconds());
-    assert_eq!(0u32, duration.nano())
+    assert_eq!(0u32, duration.nano());
 }
