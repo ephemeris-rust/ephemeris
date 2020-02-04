5796478b6134062eca6ca3168bb3d5627ee52c58
Stephen A. Imhoff <clockwork-muse@outlook.com>
2020-02-03 19:14:24 -0800

    Remove unnecessary "boxed"

--- src/duration/test_util.rs
+++ src/duration/test_util.rs
@@ -7,5 +7,4 @@ use crate::Duration;
 pub fn arb_duration() -> impl Strategy<Value = Duration> {
     (any::<i64>(), 0..NANOSECONDS_IN_SECOND)
         .prop_map(|(seconds, nanos)| Duration::of_seconds_and_adjustment(seconds, nanos))
-        .boxed()
 }
