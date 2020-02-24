e08c0a306e9755a0ffdb0301ffd6e4d340ccf899
Stephen A. Imhoff <clockwork-muse@outlook.com>
2020-02-23 19:59:41 -0800

    implement duration add

--- src/duration/test_util.rs
+++ src/duration/test_util.rs
@@ -92,6 +92,46 @@ pub fn arb_duration_underflow_units(unit_nanos: i64) -> impl Strategy<Value = (D
         })
 }
 
+fn to_duration(nanos: i128) -> Duration {
+    let seconds = (nanos / NANOSECONDS_IN_SECOND as i128) as i64;
+    let nanos_in_second = (nanos % NANOSECONDS_IN_SECOND as i128) as i64;
+    Duration::of_seconds_and_adjustment(seconds, nanos_in_second)
+}
+
+pub fn duration_total() -> impl Strategy<Value = (Duration, Duration, Duration)> {
+    (MIN_NANOS..=MAX_NANOS)
+        .prop_flat_map(|nanos| {
+            let upper = MAX_NANOS - if nanos > 0 { nanos } else { 0 };
+            let lower = MIN_NANOS - if nanos < 0 { nanos } else { 0 };
+            (Just(nanos), lower..=upper)
+        })
+        .prop_map(|(left, right)| {
+            (
+                to_duration(left),
+                to_duration(right),
+                to_duration(left + right),
+            )
+        })
+}
+
+pub fn arb_duration_overflow() -> impl Strategy<Value = (Duration, Duration)> {
+    (MAX_NANOS - i64::MAX as i128..=MAX_NANOS)
+        .prop_flat_map(|nanos| {
+            let lower = MAX_NANOS + 1 - nanos;
+            (Just(nanos), lower..=MAX_NANOS)
+        })
+        .prop_map(|(a, b)| (to_duration(a), to_duration(b)))
+}
+
+pub fn arb_duration_underflow() -> impl Strategy<Value = (Duration, Duration)> {
+    (MIN_NANOS..MIN_NANOS - i64::MIN as i128)
+        .prop_flat_map(|nanos| {
+            let upper = MIN_NANOS - 1 - nanos;
+            (Just(nanos), MIN_NANOS..=upper)
+        })
+        .prop_map(|(a, b)| (to_duration(a), to_duration(b)))
+}
+
 pub fn next(duration: Duration) -> Duration {
     match (duration.seconds(), duration.nano() as i64 + 1) {
         (seconds, NANOSECONDS_IN_SECOND) => Duration::of_seconds(seconds + 1),
