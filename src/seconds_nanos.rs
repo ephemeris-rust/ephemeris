0ef68b6b3ac29d5454347b5c230b213cef1a412e
Stephen A. Imhoff <clockwork-muse@outlook.com>
2022-03-20 23:59:01 +0000

    Add const to seconds_nanos

--- src/seconds_nanos.rs
+++ src/seconds_nanos.rs
@@ -2,14 +2,19 @@ use std::i64;
 
 use crate::constants::*;
 
-pub fn of_seconds_and_adjustment_checked(seconds: i64, nano_adjustment: i64) -> Option<(i64, u32)> {
+pub const fn of_seconds_and_adjustment_checked(
+    seconds: i64,
+    nano_adjustment: i64,
+) -> Option<(i64, u32)> {
     let (second_adjustment, nanos) = seconds_and_nanos(nano_adjustment);
-    seconds
-        .checked_add(second_adjustment)
-        .map(|total_seconds| (total_seconds, nanos))
+    // TODO: switch back to map when constant is stable
+    match seconds.checked_add(second_adjustment) {
+        None => None,
+        Some(total_seconds) => Some((total_seconds, nanos)),
+    }
 }
 
-pub fn seconds_and_nanos(nanoseconds: i64) -> (i64, u32) {
+pub const fn seconds_and_nanos(nanoseconds: i64) -> (i64, u32) {
     let (base_adjustment, base_nanos) = (
         nanoseconds / NANOSECONDS_IN_SECOND,
         nanoseconds % NANOSECONDS_IN_SECOND,
@@ -20,7 +25,8 @@ pub fn seconds_and_nanos(nanoseconds: i64) -> (i64, u32) {
 }
 
 // A second adjustment for when nanoseconds are within 1 step, instead of unbounded.
-pub fn carry_and_nanos(nanoseconds: i64) -> (i64, u32) {
+pub const fn carry_and_nanos(nanoseconds: i64) -> (i64, u32) {
+    assert!(nanoseconds > -NANOSECONDS_IN_SECOND && nanoseconds < 2 * NANOSECONDS_IN_SECOND);
     if nanoseconds < 0 {
         (-1, (nanoseconds + NANOSECONDS_IN_SECOND) as u32)
     } else if nanoseconds >= NANOSECONDS_IN_SECOND {
